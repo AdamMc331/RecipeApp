d1b8f23925ceb897cd3dc906a3f44a001895db7d
Adam McNeilly <amcneilly331@gmail.com>
2023-06-28 19:59:32 -0400

    Adding iOS project.

---
+++ recipeAppiOS/recipeAppiOS/recipeAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeAppiOSApp.swift
+//  recipeAppiOS
+//
+//  Created by Adam McNeilly on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct recipeAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
