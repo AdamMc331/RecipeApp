475ce0d587ec1efcdb5f76f134fc21bfd4dea2e4
Adam McNeilly <amcneilly331@gmail.com>
2023-06-30 14:52:27 -0400

    Supporting navigation via top app bar!!!

--- shared/src/commonMain/kotlin/com/adammcneilly/recipe/shared/RecipeApp.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/recipe/shared/RecipeApp.kt
@@ -1,5 +1,14 @@
 package com.adammcneilly.recipe.shared
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.slack.circuit.backstack.rememberSaveableBackStack
@@ -13,6 +22,7 @@ import com.slack.circuit.foundation.rememberCircuitNavigator
  * This is the main entry point into the application, showing the root screen and handles
  * the most high level navigation support.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeApp(
     modifier: Modifier = Modifier,
@@ -37,10 +47,37 @@ fun RecipeApp(
             // screen (probably just close the app?)
         }
 
-        NavigableCircuitContent(
-            navigator = navigator,
-            backstack = backstack,
-            modifier = modifier,
-        )
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            text = "Recipe App",
+                        )
+                    },
+                    navigationIcon = {
+                        if (backstack.size > 1) {
+                            IconButton(
+                                onClick = {
+                                    navigator.pop()
+                                },
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.ArrowBack,
+                                    contentDescription = "Back",
+                                )
+                            }
+                        }
+                    },
+                )
+            },
+        ) { paddingValues ->
+            NavigableCircuitContent(
+                navigator = navigator,
+                backstack = backstack,
+                modifier = modifier
+                    .padding(paddingValues),
+            )
+        }
     }
 }
