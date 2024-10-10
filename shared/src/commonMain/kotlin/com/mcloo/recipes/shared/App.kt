7c3a7348f14d0499c3c2ca3138823a1c1d32dbcc
Adam McNeilly <amcneilly331@gmail.com>
2024-10-09 20:17:56 -0400

    Adding RecipeListItemCard.

--- shared/src/commonMain/kotlin/com/mcloo/recipes/shared/App.kt
+++ shared/src/commonMain/kotlin/com/mcloo/recipes/shared/App.kt
@@ -1,66 +1,25 @@
 package com.mcloo.recipes.shared
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.mcloo.recipes.shared.theme.RecipeTheme
-import org.jetbrains.compose.resources.painterResource
+import com.mcloo.recipes.shared.ui.components.RecipeListItemCard
+import com.mcloo.recipes.shared.ui.displaymodels.RecipeSummaryDisplayModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun App() {
     RecipeTheme {
-        var showContent by remember {
-            mutableStateOf(false)
-        }
-
-        Surface {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Button(
-                    onClick = {
-                        showContent = !showContent
-                    },
-                ) {
-                    Text(
-                        text = "Click me!",
-                    )
-                }
-
-                AnimatedVisibility(showContent) {
-                    val greeting = remember {
-                        Greeting().greet()
-                    }
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                    ) {
-                        Image(
-                            painter = painterResource(Res.drawable.compose_multiplatform),
-                            contentDescription = null,
-                        )
-                        Text(
-                            text = "Compose: $greeting",
-                        )
-                    }
-                }
-            }
-        }
+        RecipeListItemCard(
+            recipe = RecipeSummaryDisplayModel(
+                name = "Spicy Arrabiata Penne",
+                imageUrl = "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
+            ),
+            modifier = Modifier
+                .padding(16.dp),
+        )
     }
 }
---
+++ shared/src/commonMain/kotlin/com/mcloo/recipes/shared/ui/components/RecipeListItemCard.kt
@@ -0,0 +1,48 @@
+package com.mcloo.recipes.shared.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.mcloo.recipes.shared.ui.displaymodels.RecipeSummaryDisplayModel
+
+@Composable
+fun RecipeListItemCard(
+    recipe: RecipeSummaryDisplayModel,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        shape = MaterialTheme.shapes.large,
+        modifier = modifier,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1F),
+        ) {
+            AsyncImage(
+                model = recipe.imageUrl,
+                contentDescription = "${recipe.name} Image",
+                modifier = Modifier
+                    .fillMaxSize(),
+            )
+
+            Text(
+                text = recipe.name,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomStart),
+            )
+        }
+    }
+}
