9f057addc1d931454dc83ac2a6738d29d41e7d35
Adam McNeilly <amcneilly331@gmail.com>
2024-10-30 20:21:41 -0400

    Removing magic number.

--- shared/src/commonMain/kotlin/com/mcloo/recipes/shared/recipelist/RecipeListContent.kt
+++ shared/src/commonMain/kotlin/com/mcloo/recipes/shared/recipelist/RecipeListContent.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +31,7 @@ fun RecipeListContent(
         OutlinedTextField(
             value = searchQuery,
             onValueChange = onSearchQueryChanged,
-            shape = RoundedCornerShape(50),
+            shape = CircleShape,
             placeholder = {
                 Text(
                     text = "Search",
