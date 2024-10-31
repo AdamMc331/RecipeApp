ed650ca3483c677148202a1e2cad1fae19be9f6a
Adam McNeilly <amcneilly331@gmail.com>
2024-10-30 20:20:29 -0400

    Adding search networking.

---
+++ shared/src/commonMain/kotlin/com/mcloo/recipes/shared/UseDebounce.kt
@@ -0,0 +1,35 @@
+package com.mcloo.recipes.shared
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * Inspiration: https://mohammedev.hashnode.dev/usedebounce-in-androids-jetpack-compose
+ */
+@Composable
+fun <T> T.useDebounce(
+    delayMillis: Long = 300L,
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    onChange: (T) -> Unit,
+): T {
+    val state by rememberUpdatedState(this)
+
+    DisposableEffect(state) {
+        val job = coroutineScope.launch {
+            delay(delayMillis)
+            onChange(state)
+        }
+
+        onDispose {
+            job.cancel()
+        }
+    }
+
+    return state
+}
--- shared/src/commonMain/kotlin/com/mcloo/recipes/shared/recipelist/RecipeListPresenter.kt
+++ shared/src/commonMain/kotlin/com/mcloo/recipes/shared/recipelist/RecipeListPresenter.kt
@@ -1,16 +1,18 @@
 package com.mcloo.recipes.shared.recipelist
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import com.mcloo.recipes.shared.data.RecipeService
 import com.mcloo.recipes.shared.recipedetail.RecipeDetailScreen
 import com.mcloo.recipes.shared.ui.displaymodels.RecipeSummaryDisplayModel
+import com.mcloo.recipes.shared.useDebounce
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
+import kotlinx.coroutines.launch
 
 class RecipeListPresenter(
     private val recipeService: RecipeService,
@@ -18,13 +20,18 @@ class RecipeListPresenter(
 ) : Presenter<RecipeListScreen.State> {
     @Composable
     override fun present(): RecipeListScreen.State {
+        val coroutineScope = rememberCoroutineScope()
         var searchQuery by remember { mutableStateOf("") }
         var recipes by remember { mutableStateOf(emptyList<RecipeSummaryDisplayModel>()) }
 
-        LaunchedEffect(Unit) {
+        searchQuery.useDebounce(
+            coroutineScope = coroutineScope,
+        ) { query ->
             // TODO: Add error handling
-            recipeService.getRecipesByName("chicken").onSuccess { result ->
-                recipes = result.map(::RecipeSummaryDisplayModel)
+            coroutineScope.launch {
+                recipeService.getRecipesByName(query).onSuccess { result ->
+                    recipes = result.map(::RecipeSummaryDisplayModel)
+                }
             }
         }
 
